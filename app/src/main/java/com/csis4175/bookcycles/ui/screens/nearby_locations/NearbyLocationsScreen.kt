3ab3f09c54ee943cf50e8f4b7c03a635a4f8ba46
Wimalaratne <wimalaratnek@student.douglascollege.ca>
2024-11-14 14:49:36 -0800

    chore(nearby-books): Remove unused imports

--- app/src/main/java/com/csis4175/bookcycles/ui/screens/nearby_locations/NearbyLocationsScreen.kt
+++ app/src/main/java/com/csis4175/bookcycles/ui/screens/nearby_locations/NearbyLocationsScreen.kt
@@ -1,25 +1,13 @@
 package com.csis4175.bookcycles.ui.screens.nearby_locations
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.csis4175.bookcycles.R
 import com.csis4175.bookcycles.ui.components.InputButton
 import com.csis4175.bookcycles.ui.components.InputMap
 
